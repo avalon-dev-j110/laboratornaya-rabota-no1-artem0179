6440bb17fe2bbecf4a2e4912b1ba2dd2a8e9e1eb
рпи <@192.168.1.114>
2020-04-14 19:59:20 +0300

    Lab №1 done.

--- src/ru/avalon/java/dev/j10/labs/commons/Address.java
+++ src/ru/avalon/java/dev/j10/labs/commons/Address.java
@@ -1,3 +1,7 @@
+package ru.avalon.java.dev.j10.labs.commons;
+
+
+
 /*
  * TODO(Студент): Создайте класс Address.
  *
@@ -13,3 +17,65 @@
  * 4. Подумайте над тем, какие методы должны быть объявлены
  *    в классе.
  */
+
+public class Address {
+   private String country;
+   private String city;
+   private String street;
+   private int houseNumber;
+   private int flatNumber;
+    
+         
+   public Address (String country, String city, String street, int houseNumber,
+           int flatNumber){
+   this.country = country;
+   this.city = city;
+   this.street = street;
+   this.houseNumber = houseNumber;
+   this.flatNumber = flatNumber;
+   }
+    
+   public void setCountry(String country){
+       this.country = country;
+   }
+   
+   public String getCountry(){
+       return country;
+   }
+           
+   public void setCity(String city){
+       this.city = city;
+   }
+   
+   public String getCity(){
+       return city;
+   }
+           
+   public void setStreet(String street){
+       this.street = street;
+   }
+   
+   public String getStreet(){
+       return street;
+   }
+   
+   public void setHouseNumber(int houseNumber){
+       this.houseNumber = houseNumber;
+   }
+   
+   public int getHouseNumber(){
+       return houseNumber;
+   }
+
+   public void setFlatNumber(int flatNumber){
+       this.flatNumber = flatNumber;
+   }
+   
+   public int getFlatNumber(){
+       return flatNumber; 
+   }
+   
+   
+   
+   
+}
--- src/ru/avalon/java/dev/j10/labs/models/Person.java
+++ src/ru/avalon/java/dev/j10/labs/models/Person.java
@@ -1,42 +1,46 @@
 package ru.avalon.java.dev.j10.labs.models;
 
+import java.time.LocalDate;
+import ru.avalon.java.dev.j10.labs.commons.Address;
+
 /**
  * Представление о человеке.
  * <p>
- * С точки зрения задания человек представляется как сущность,
- * наделённая:
+ * С точки зрения задания человек представляется как сущность, наделённая:
  * <ol>
- *     <li>паспортными данными;
- *     <li>пропиской по месту жительства.
+ * <li>паспортными данными;
+ * <li>пропиской по месту жительства.
  * </ol>
  */
 public class Person {
-    private String fullName;
 
-    public Person(String fullName) {
-        this.fullName = fullName;
-        
+    private Passport passportData;
+    private Address livingAddress;
 
-        
+
+    public Person(String name, String middleName, String secondName, String surname,
+            LocalDate birthDate, int passportNumber, LocalDate receiveDate,
+            String receivePlace, String country, String city, String street,
+            int houseNumber, int flatNumber) {
+
+        passportData = new Passport(name, middleName, secondName, surname, birthDate, passportNumber,
+                receiveDate, receivePlace);
+        livingAddress = new Address(country, city, street, houseNumber, flatNumber);
+
     }
-    
-    
-    
 
     /**
      * Возврвщает полное имя человека.
      * <p>
-     * Если у человека есть Имя, Фамилия и Отчество, то
-     * возвращет Имя, Фимилию и Отчество, разделённые пробелом.
+     * Если у человека есть Имя, Фамилия и Отчество, то возвращет Имя, Фимилию и
+     * Отчество, разделённые пробелом.
      * <p>
-     * Если у человека нет Отчества, но есть второе имя, то
-     * возвращает Имя, Первую букву второго имени, и Фамилию,
-     * разделённые пробелом. После Инициала второго имени
-     * должна стоять точка. Например, "Джером К. Джером".
+     * Если у человека нет Отчества, но есть второе имя, то возвращает Имя,
+     * Первую букву второго имени, и Фамилию, разделённые пробелом. После
+     * Инициала второго имени должна стоять точка. Например, "Джером К. Джером".
      * <p>
-     * Если у человека нет ни Отчества ни Второго имени, а
-     * есть только Имя и Фамилия, то возвращает их, разделённые
-     * пробелом.
+     * Если у человека нет ни Отчества ни Второго имени, а есть только Имя и
+     * Фамилия, то возвращает их, разделённые пробелом.
      *
      * @return имя человека в виде строки.
      */
@@ -44,21 +48,35 @@ public class Person {
         /*
          * TODO(Студент): Закончить определение метода 'getFullName()' класса 'Person'
          */
-        return fullName;
+
+        if (passportData.getSecondName() == null) {
+            String fullName = passportData.getName() + " " + passportData.getMiddleName()
+                    + " " + passportData.getSurname();
+            return fullName;
+        } else {
+            String fullName = passportData.getName() + " " + passportData.getSecondName().charAt(0)
+                    + "." + " " + passportData.getSurname();
+            return fullName;
+        }
+
     }
 
     /**
      * Возвращает адрес, по которому проживает человек.
      * <p>
-     * Возвращаемый адрес соответствует месту постоянной
-     * регистрации человека, согласно паспортным данным.
+     * Возвращаемый адрес соответствует месту постоянной регистрации человека,
+     * согласно паспортным данным.
      *
      * @return адрес регистрации в виде строки.
      */
     public String getAddress() {
+
+        String getAddress = livingAddress.getCountry() + ", г." + livingAddress.getCity()
+                + ", " + livingAddress.getStreet() + ", " + livingAddress.getHouseNumber()
+                + "-" + livingAddress.getFlatNumber();
         /*
          * TODO(Студент): Закончить определение метода 'getAddress()' класса 'Person'
          */
-        return null;
+        return getAddress;
     }
 }
